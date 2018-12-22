8ed2383516fc9fd415f6a2b0192962cc93013af2
Mikhail Nakonechnyi <mykhail.nakonechnyi@gmail.com>
2018-12-22 22:08:23 +0000

    was added lesson10

--- 2_cs50_groups/cs1/lesson8/binary.cpp
+++ 2_cs50_groups/cs1/lesson8/binary.cpp
@@ -7,7 +7,7 @@ int binarySearch (int arr[], int l, int r, int x)
     if (r >= l)
     {
         int mid = l + (r - l) / 2;
-        cout << mid << endl;
+        //cout << mid << endl;
         if (arr[mid] == x) return mid;
 
         if ( arr[mid] > x )
