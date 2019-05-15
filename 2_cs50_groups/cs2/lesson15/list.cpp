02a9e071b9edf5d4171ee0c60dfdd063ec72a5dd
Mikhail Nakonechnyi <mykhail.nakonechnyi@gmail.com>
2019-05-15 15:36:01 +0000

    was completed list structure

--- 2_cs50_groups/cs2/lesson15/list.cpp
+++ 2_cs50_groups/cs2/lesson15/list.cpp
@@ -10,17 +10,23 @@ typedef struct node
     int value;
     struct node* next;
 } node_t;//node_t
+
 void push(node_t * head, int value);
 void push_start(node_t ** head, int value);
 void printList(node_t * head);
+bool search (node_t* head, int value);
 int main(void){
+    // node_t* head1;// = NULL;
     node_t* head = NULL;
+    // cout << head1 << endl;
+    // cout << head2 << endl;
     head = (node_t*)malloc(sizeof(node));
     if (head == NULL) {
         return 1;
     }
     head->value=1;
     // head->next = NULL;
+
     head->next = (node_t*)malloc(sizeof(node));
     head->next->value = 2;
     head->next->next = NULL;
@@ -29,8 +35,21 @@ int main(void){
     push(head,3);
     printList(head);
     cout << "----------------" << endl;
+    push_start(&head,0);
+    printList(head);
+    cout << "----------------" << endl;
+    int x = 3;
+    cout << "search of element " << x << " in list gives " << search(head, x) << endl;
 
     return 0;
+}
+void push_start(node_t ** head, int value){
+    node* new_node;
+    new_node = (node_t*)malloc(sizeof(node));
+
+    new_node->value = value;
+    new_node->next = *head;
+    *head = new_node;
 }
 
 void push(node_t * head, int value){
@@ -50,4 +69,18 @@ void printList(node_t* head) {
         cout << "Address: " << (void*)current->next << endl;
         current = current->next;
     }
+}
+
+bool search (node_t* head, int value)
+{
+  node_t* ptr = head;
+  while (ptr != NULL)
+  {
+    if(ptr -> value == value)
+    {
+      return true;
+    }
+    ptr = ptr -> next;
+  }
+  return false;
 }
